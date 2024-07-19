fa1ed09ddd510a376d214dc45df4af98a904e8e8
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-19 18:53:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+n = 1
+y = 0
+for i in range(1,6):
+    for j in range(1,4):
+        print(n,y,end=' ')
+        # print(y, end=' ')
+
+    print('')
+# output
+'''
+1 0 1 0 1 0 
+1 0 1 0 1 0 
+1 0 1 0 1 0 
+1 0 1 0 1 0 
+1 0 1 0 1 0 
+'''
